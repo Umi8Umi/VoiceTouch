e273ee6b95e2b178f4c1e2143772b8df0fd1264d
Kang Sun <675355574@qq.com>
2019-11-13 21:41:59 -0500

    update r.sh so app can be launched

--- r.sh
+++ r.sh
@@ -9,6 +9,7 @@ adb uninstall io.voxhub.accessibility.voicetouch
 sleep 1
 adb install ./app/build/outputs/apk/app-debug.apk
 sleep 1
-adb shell am start -n io.voxhub.accessibility.voicetouch/io.voxhub.accessibility.app.SimpleActivity
+
+adb shell am start -n io.voxhub.accessibility.voicetouch/io.voxhub.accessibility.voicetouch.SimpleActivity
 
 # adb shell am start -n io.voxhub.accessibility.servicecode/io.voxhub.accessibility.servicecode.MyAccessibilityService
