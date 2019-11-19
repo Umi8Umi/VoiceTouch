a0198bcfecfd43068e3fc043b2d3e0e373fddbf7
Kang Sun <675355574@qq.com>
2019-11-19 09:51:35 -0500

    migrate the whole project to gradle 5.4, everything can work now

--- r.sh
+++ r.sh
@@ -3,11 +3,13 @@
 ./gradlew assembleDebug
 adb uninstall io.voxhub.accessibility.servicecode
 sleep 1
-adb install ./service/build/outputs/apk/service-debug.apk
-sleep 1
+
+#adb install ./service/build/outputs/aar/service-debug.aar
+#sleep 1
+
 adb uninstall io.voxhub.accessibility.voicetouch
 sleep 1
-adb install ./app/build/outputs/apk/app-debug.apk
+adb install ./app/build/outputs/apk/debug/app-debug.apk
 sleep 1
 
 adb shell am start -n io.voxhub.accessibility.voicetouch/io.voxhub.accessibility.voicetouch.SimpleActivity
